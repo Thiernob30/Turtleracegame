aa5df7fe316abfebad4796d2f302f3b9bfeac9b0
Thierno Bah <thiern.ba30@gmail.com>
2024-02-12 14:42:11 -0500

    initial commit

---
+++ turtle racing game.py
@@ -0,0 +1,20 @@
+#author
+#date
+#Desrcription
+#revision history
+#Name   Date     Desc
+#scl    2/12/24     initial code base
+#
+#
+
+
+
+import time
+import turtle
+import random as rd
+
+start_x = -200
+start_y = 150
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
